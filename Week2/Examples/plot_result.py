07c32821fef4a05f4ab7c32d18c427b31e132ac3
cristian <crisstrink@gmail.com>
2017-06-09 08:55:20 -0300

    estruturação de arquivos

--- Week2/Examples/plot_result.py
+++
@@ -1,7 +0,0 @@
-from Helper_functions import plot_images,plot_example_errors,print_confusion_matrix
-
-def show_example_errors():
-    plot_example_errors(data, session, correct_pred, pred , feed_dict_test)
-
-def show_confusion_matrix():
-    print_confusion_matrix(data, session, pred, feed_dict_test, n_classes)
